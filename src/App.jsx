5d2c90fdc3646983e1e4c26b38778ef8e5c8e341
Ahmad Ali <xevenahmad@gmail.com>
2024-10-13 22:27:09 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default App
